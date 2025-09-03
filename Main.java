850851625fa5a46b2298800ad9042dc2c02062d5
jessedtran <jessedanieltran@gmail.com>
2025-09-02 20:57:37 -0700

    Commit 2

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
+
+     static boolean condition;
     public static void main(String[] args) {
+        condition = false;
         System.out.println("Hello, World!");
     }
 }
---
+++ README.md
@@ -0,0 +1,4 @@
+Name: Jesse Tran
+Github Repo: https://github.com/jessedtran/git_ready
+Screencast Video:
+Software:
